2a204ff78764c4ede60cb968b06aca0df2d70c54
memeSong <memehsforever@gmial.com>
2021-03-02 15:01:49 +0900

    Add Showing score#2

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -4,7 +4,7 @@ import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
-import {Button} from '@material-ui/core';
+import { Button } from '@material-ui/core';
 import './main.css';
 
 import axios from 'axios';
@@ -31,7 +31,7 @@ function Main(): any {
         let selectVal = Number(event.target.value)
         setCrp(selectVal);
     };
-  
+
     const handleLDHChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         let selectVal = Number(event.target.value)
         setLdh(selectVal);
@@ -42,71 +42,71 @@ function Main(): any {
         setHemo(selectVal);
     };
 
-    const handleTotalScore = (event:any) => {
-        if(age === -1){
+    const handleTotalScore = (event: any) => {
+        if (age === -1) {
             console.log(" 나이를 입력하세요. ")
             return;
         }
-        if(crp === -1){
+        if (crp === -1) {
             console.log(" C-reactive protein을 입력하세요. ")
             return;
         }
 
-        if(ldh === -1){
+        if (ldh === -1) {
             console.log(" Lactate dehydrogenase을 입력하세요. ")
             return;
         }
 
-        if(hemo === -1){
+        if (hemo === -1) {
             console.log(" hemoglobin을 입력하세요. ")
             return;
         }
 
         let totalScore = age + crp + ldh + hemo;
         setTotalScore(totalScore);
-        if(totalScore >= 9){
+        if (totalScore >= 9) {
             setResultString("중증 폐렴 전이 가능성이 높은 환자입니다.");
-        }else{
+        } else {
             setResultString("중증 폐렴 전이 가능성이 낮은 환자입니다.");
         }
-        
+
         setResultScoreString(`위험도 지수: ${totalScore}`);
 
-        const patientDtoObj  = {
-            age:age,
-            crp:crp,
-            ldh:ldh,
-            hemo:hemo
+        const patientDtoObj = {
+            age: age,
+            crp: crp,
+            ldh: ldh,
+            hemo: hemo
         }
 
         console.log("patientDtoObj >>>>>> ", patientDtoObj)
 
-        axios.post(comm.SERVER_URL+"/patient", patientDtoObj).then(res => {
+        axios.post(comm.SERVER_URL + "/patient", patientDtoObj).then(res => {
             console.log(' res >> ', res);
         });
     }
-    
-    const handleInit = (ev:any) => {
+
+    const handleInit = (ev: any) => {
         setAge(-1);
         setCrp(-1);
         setLdh(-1);
         setHemo(-1);
         setResultString("");
         setResultScoreString("");
-    } 
+    }
 
 
     return (
-        <div style={{display:'flex', flexDirection:'column', margin:25 }}>
-            <div style={{marginTop:10}}>환자 검진 정보를 체크해 주세요</div>
-            <FormControl component="fieldset" style={{marginTop:20}}>
+        <div style={{ display: 'flex', flexDirection: 'column', margin: 25 }}>
+            <div style={{ marginTop: 10 }}>환자 검진 정보를 체크해 주세요</div>
+            <FormControl component="fieldset" style={{ marginTop: 20 }}>
                 <div className='label-div'>
                     <span className='label-span'>Age(years)</span>
                     <span className='score-span'>{age === -1 ? "" : `score: ${age}`}</span>
                 </div>
 
-                <div style={{display:'flex', flexDirection:'row'}}>
-                    <RadioGroup aria-label="age" name="age" value={age} onChange={handleAgeChange} style={{padding:5}}>
+                <div style={{ display: 'flex', flexDirection: 'row' }}>
+                    <RadioGroup aria-label="age" name="age" value={age} onChange={handleAgeChange} style={{ padding: 5 }}>
                         <FormControlLabel value={0} control={<Radio />} label="50 미만" />
                         <FormControlLabel value={4} control={<Radio />} label="50대(50-59)" />
                         <FormControlLabel value={5} control={<Radio />} label="60대(60-69)" />
@@ -121,47 +121,47 @@ function Main(): any {
 
             </FormControl>
 
-            <FormControl component="fieldset" style={{marginTop:15}}>
+            <FormControl component="fieldset" style={{ marginTop: 15 }}>
                 <div className='label-div'>
                     <span className='label-span'>C-reactive protein(mg/dL)</span>
                     <span className='score-span'>{crp === -1 ? "" : `score: ${crp}`}</span>
                 </div>
-                <RadioGroup aria-label="crp" name="crp" value={crp} onChange={handleCRPChange} style={{padding:5}}>
+                <RadioGroup aria-label="crp" name="crp" value={crp} onChange={handleCRPChange} style={{ padding: 5 }}>
                     <FormControlLabel value={0} control={<Radio />} label="< 1.4" />
                     <FormControlLabel value={3} control={<Radio />} label=">= 1.4" />
                 </RadioGroup>
             </FormControl>
 
-            <FormControl component="fieldset" style={{marginTop:15}}>
+            <FormControl component="fieldset" style={{ marginTop: 15 }}>
                 <div className='label-div'>
                     <span className='label-span'>Lactate dehydrogenase(U/L)</span>
                     <span className='score-span'>{ldh === -1 ? "" : `score: ${ldh}`}</span>
                 </div>
-                <RadioGroup aria-label="ldh" name="ldh" value={ldh} onChange={handleLDHChange} style={{padding:5}}>
+                <RadioGroup aria-label="ldh" name="ldh" value={ldh} onChange={handleLDHChange} style={{ padding: 5 }}>
                     <FormControlLabel value={0} control={<Radio />} label="< 500" />
                     <FormControlLabel value={2} control={<Radio />} label="500-700" />
                     <FormControlLabel value={4} control={<Radio />} label=">= 700" />
                 </RadioGroup>
             </FormControl>
 
-            <FormControl component="fieldset" style={{marginTop:15}}>
+            <FormControl component="fieldset" style={{ marginTop: 15 }}>
                 <div className='label-div'>
                     <span className='label-span'>Hemoglobin(g/dL)</span>
                     <span className='score-span'>{hemo === -1 ? "" : `score: ${hemo}`}</span>
                 </div>
-                <span style={{fontWeight:'bold', color:'black', fontSize:17.5}}>Hemoglobin(g/dL)</span>
-                <RadioGroup aria-label="hemo" name="hemo" value={hemo} onChange={handleHemoChange} style={{padding:5}}>
+                <span style={{ fontWeight: 'bold', color: 'black', fontSize: 17.5 }}>Hemoglobin(g/dL)</span>
+                <RadioGroup aria-label="hemo" name="hemo" value={hemo} onChange={handleHemoChange} style={{ padding: 5 }}>
                     <FormControlLabel value={0} control={<Radio />} label="< 13.3" />
                     <FormControlLabel value={3} control={<Radio />} label=">= 13.3" />
                 </RadioGroup>
             </FormControl>
             <div className='btn-div'>
                 <Button color="primary" variant="contained" size="medium" onClick={handleTotalScore}>결과 보기</Button>
-                <Button color="secondary" variant="outlined" size="medium" onClick={handleInit} style={{marginLeft:15}}>초기화</Button>
+                <Button color="secondary" variant="outlined" size="medium" onClick={handleInit} style={{ marginLeft: 15 }}>초기화</Button>
             </div>
             <div className='resultstr-div'>
                 {totalScore >= 9 ? <p style={{ color: 'red', fontWeight: "bold" }}>{resultScoreString}</p> : <p style={{ color: 'green' }}>{resultScoreString}</p>}
-                {totalScore >= 9 ? <p style={{color:'red', fontWeight:"bold"}}>{resultString}</p> : <p style={{color:'green'}}>{resultString}</p>}
+                {totalScore >= 9 ? <p style={{ color: 'red', fontWeight: "bold" }}>{resultString}</p> : <p style={{ color: 'green' }}>{resultString}</p>}
             </div>
 
         </div>
--- src/screens/main2.tsx
+++ src/screens/main2.tsx
@@ -6,7 +6,7 @@ import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 import { Button } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
-
+import './main.css';
 import axios from 'axios';
 import comm from '../common';
 import { Link, useHistory } from 'react-router-dom';
@@ -24,15 +24,13 @@ function Main2(): any {
     const [realLdh, setRealldh] = React.useState(-1);
     const [realHemo, setRealhemo] = React.useState(-1);
 
-
-
     const [totalScore, setTotalScore] = React.useState(0);
     const [resultString, setResultString] = React.useState("");
     const [resultScoreString, setResultScoreString] = React.useState("");
 
-    const crpInput : any = useRef<HTMLDivElement>(null);
-    const ldhInput : any = useRef<HTMLDivElement>();
-    const hemoInput : any = useRef<HTMLDivElement>();
+    const crpInput: any = useRef<HTMLDivElement>(null);
+    const ldhInput: any = useRef<HTMLDivElement>();
+    const hemoInput: any = useRef<HTMLDivElement>();
 
     const handleAgeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         let selectVal = Number(event.target.value)
@@ -43,11 +41,11 @@ function Main2(): any {
         setRealcrp(Number(event.target.value));
         let inputVal = Number(event.target.value);
         console.log(" inputVal CRP >> ", inputVal);
-        if(inputVal < 1.4){
+        if (inputVal < 1.4) {
             inputVal = 0;
-        }else{
+        } else {
             inputVal = 3;
-        }  
+        }
         setCrp(inputVal);
 
     };
@@ -56,11 +54,11 @@ function Main2(): any {
         setRealldh(Number(event.target.value));
         let inputVal = Number(event.target.value);
         console.log(" inputVal LDH >> ", inputVal);
-        if(inputVal < 500){
+        if (inputVal < 500) {
             inputVal = 0;
-        }else if(inputVal < 700){
+        } else if (inputVal < 700) {
             inputVal = 2;
-        }else{
+        } else {
             inputVal = 4;
         }
         setLdh(inputVal);
@@ -70,9 +68,9 @@ function Main2(): any {
         setRealhemo(Number(event.target.value));
         let inputVal = Number(event.target.value);
         console.log(" inputVal Hemo >> ", inputVal);
-        if(inputVal < 13.3){
+        if (inputVal < 13.3) {
             inputVal = 0;
-        }else{
+        } else {
             inputVal = 3;
         }
         setHemo(inputVal);
@@ -97,7 +95,7 @@ function Main2(): any {
             console.log(" hemoglobin을 입력하세요. ")
             return;
         }
-        
+
         let totalScore = age + crp + ldh + hemo;
         setTotalScore(totalScore);
         if (totalScore >= 9) {
@@ -108,16 +106,16 @@ function Main2(): any {
 
         setResultScoreString(`위험도 지수: ${totalScore}`);
 
-        const patientDtoObj  = {
-            age:age,
-            crp:realCrp,
-            ldh:realLdh,
-            hemo:realHemo
+        const patientDtoObj = {
+            age: age,
+            crp: realCrp,
+            ldh: realLdh,
+            hemo: realHemo
         }
 
         console.log("patientDtoObj >>>>>> ", patientDtoObj)
 
-        axios.post(comm.SERVER_URL+"/patient", patientDtoObj).then(res => {
+        axios.post(comm.SERVER_URL + "/patient", patientDtoObj).then(res => {
             console.log(' res >> ', res);
         });
 
@@ -133,10 +131,13 @@ function Main2(): any {
     }
 
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', margin: 25, width:360}}>
+        <div style={{ display: 'flex', flexDirection: 'column', margin: 25, width: 360 }}>
             <div style={{ marginTop: 10 }}>환자 검진 정보를 입력해 주세요</div>
             <FormControl component="fieldset" style={{ marginTop: 20 }}>
-                <FormLabel component="legend" style={{ fontWeight: 'bold', color: 'black', fontSize: 17.5 }}>Age(years)</FormLabel>
+                <div className='label-div'>
+                    <span className='label-span'>Age(years)</span>
+                    <span className='score-span'>{age === -1 ? "" : `score: ${age}`}</span>
+                </div>
                 <RadioGroup aria-label="age" name="age" value={age} onChange={handleAgeChange} style={{ padding: 5 }}>
                     <FormControlLabel value={0} control={<Radio />} label="50 미만" />
                     <FormControlLabel value={4} control={<Radio />} label="50대(50-59)" />
@@ -147,7 +148,11 @@ function Main2(): any {
             </FormControl>
 
             <FormControl component="fieldset" style={{ marginTop: 15 }}>
-                <FormLabel component="legend" style={{ fontWeight: 'bold', color: 'black', fontSize: 17.5, marginBottom: 15 }}>C-reactive protein(mg/dL)</FormLabel>
+                <div className='label-div'>
+                    <span className='label-span'>C-reactive protein(mg/dL)</span>
+                    <span className='score-span'>{crp === -1 ? "" : `score: ${crp}`}</span>
+                </div>
+                <div style={{height:12}}></div>
                 <TextField
                     id="crp"
                     label="mg/dL"
@@ -156,16 +161,21 @@ function Main2(): any {
                         shrink: true,
                     }}
                     variant="outlined"
-                    style={{width:250}}
+                    style={{ width: 250 }}
                     onChange={handleCRPChange}
                     inputRef={crpInput}
                     // ref={crpInput}
                     placeholder="ex) 1.4"
-                />  
+                />
             </FormControl>
 
             <FormControl component="fieldset" style={{ marginTop: 15 }}>
-                <FormLabel component="legend" style={{ fontWeight: 'bold', color: 'black', fontSize: 17.5, marginBottom: 15 }}>Lactate dehydrogenase(U/L)</FormLabel>
+                <div className='label-div'>
+                    <span className='label-span'>Lactate dehydrogenase(U/L)</span>
+                    <span className='score-span'>{ldh === -1 ? "" : `score: ${ldh}`}</span>
+                </div>
+                <div style={{height:12}}></div>
+
                 <TextField
                     id="ldh"
                     label="U/L"
@@ -174,7 +184,7 @@ function Main2(): any {
                         shrink: true,
                     }}
                     variant="outlined"
-                    style={{width:250}}
+                    style={{ width: 250 }}
                     onChange={handleLDHChange}
                     inputRef={ldhInput}
                     placeholder="ex) 550"
@@ -183,7 +193,12 @@ function Main2(): any {
             </FormControl>
 
             <FormControl component="fieldset" style={{ marginTop: 15 }}>
-                <FormLabel component="legend" style={{ fontWeight: 'bold', color: 'black', fontSize: 17.5, marginBottom: 15 }}>Hemoglobin(g/dL)</FormLabel>
+                <div className='label-div'>
+                    <span className='label-span'>Hemoglobin(g/dL)</span>
+                    <span className='score-span'>{hemo === -1 ? "" : `score: ${hemo}`}</span>
+                </div>
+                <div style={{height:12}}></div>
+
                 <TextField
                     id="ldh"
                     label="g/dL"
@@ -193,7 +208,7 @@ function Main2(): any {
                         shrink: true,
                     }}
                     variant="outlined"
-                    style={{width:250}}
+                    style={{ width: 250}}
                     onChange={handleHemoChange}
                     inputRef={hemoInput}
                     placeholder="ex) 13.3"
